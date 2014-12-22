9b885bb64e4a4d05f5cb974627fe330d941285d6
Randy Syring <randy.syring@lev12.com>
2014-12-22 13:51:35 -0500

    initial commit

---
+++ {{cookiecutter.project_namespace}}-src/readme.rst
@@ -0,0 +1,23 @@
+<short summary>
+
+{{cookiecutter.project_name}}'s Goal
+------------------------------------
+
+<fill in>
+
+Plans
+-----------
+
+<fill in>
+
+Questions & Comments
+---------------------
+
+Please visit: <mailing list or something>
+
+Current Status
+---------------
+
+Very Alpha, expect changes.
+
+
---
+++ {{cookiecutter.project_namespace}}-src/setup.py
@@ -0,0 +1,36 @@
+import os
+import os.path as osp
+
+from setuptools import setup
+
+cdir = os.path.abspath(os.path.dirname(__file__))
+README = open(osp.join(cdir, 'readme.rst')).read()
+CHANGELOG = open(osp.join(cdir, 'changelog.rst')).read()
+VERSION = open(osp.join(cdir, '{{cookiecutter.project_namespace}}', 'version.txt')).read().strip()
+
+setup(
+    name='{{cookiecutter.project_name}}',
+    version=VERSION,
+    description='<short description>',
+    author='<author>',
+    author_email='<author email>',
+    url='',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=['{{cookiecutter.project_namespace}}'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # use this for libraries; or
+        # use requirements folder/files for apps
+    ],
+    entry_points='''
+        [console_scripts]
+        {{cookiecutter.project_namespace}} = {{cookiecutter.project_namespace}}.cli:cli_entry
+    ''',
+)
