501caf7a69e6f9ea851f83bfa8d6a56f901c49bb
Randy Syring <randy.syring@level12.io>
2015-01-15 23:01:48 -0500

    move defaults to app's config file

--- {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_namespace}}-config.py
+++ {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_namespace}}-config.py
@@ -1,16 +1,6 @@
 from __future__ import unicode_literals
 
 
-class Default(object):
-    """
-        apply's to all config profiles
-    """
-    # It's tempting to turn this off to avoid the warning, but if you are storing passwords
-    # in your settings, leave this enabled and setup a keyring.  See the app's keyring related
-    # commands for help.
-    KEG_KEYRING_ENABLE = True
-
-
 class Dev(object):
     # secret key for Flask
     SECRET_KEY = ''
