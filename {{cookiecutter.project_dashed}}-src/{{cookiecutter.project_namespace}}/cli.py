213022b521796a774f89657f316fa7622d38e26b
Randy Syring <randy@thesyrings.us>
2017-07-25 18:09:15 -0400

    add logging command to cli.py

--- {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_namespace}}/cli.py
+++ {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_namespace}}/cli.py
@@ -1,10 +1,13 @@
 from __future__ import absolute_import
-from __future__ import unicode_literals
 from __future__ import print_function
+
+import logging
 
 import click
 
 from {{cookiecutter.project_namespace}}.app import {{cookiecutter.project_class}}
+
+log = logging.getLogger(__name__)
 
 
 def cli_entry():
@@ -15,6 +18,13 @@ def cli_entry():
 @click.option('--name', default='World', help='The person to greet.')
 def hello_world(name):
     click.echo('Hello {} from {{cookiecutter.project_name}}!'.format(name))
+
+
+@{{cookiecutter.project_class}}.command('log', short_help='log some messages')
+def logcmd():
+    log.info('info log message')
+    log.warning('warning log message')
+    log.error('error log message')
 
 
 if __name__ == '__main__':
