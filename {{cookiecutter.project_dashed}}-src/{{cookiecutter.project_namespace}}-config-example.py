7e30d885dfee3f5a22550a17d8613c4d5c92b580
Randy Syring <randy@thesyrings.us>
2017-08-22 13:20:41 -0400

    Create app config example instead of config itself
    That way, devs should be prompted to realize they need to keep the example updated.
    
    fixes #21 - added Celery config to DevProfile while I was at it.

--- {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_namespace}}-config-example.py
+++ {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_namespace}}-config-example.py
@@ -15,14 +15,19 @@ class DevProfile(object):
     # Needed by at least KegAuth for sending emails from the command line
     SERVER_NAME = 'localhost:5000'
 
+    CELERY = {
+        # Local rabbitmq server
+        'broker_url': 'amqp://guest@localhost:5672//',
+    }
+
 
 class TestProfile(object):
-    """ Uncomment these as needed """
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://user:pass@localhost/test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user:pass@localhost/test'
 
-    # CELERY = {
-    #     # This should be for the docker container setup in the CircleCI config.
-    #     'broker_url': 'amqp://guest@localhost:5672//',
-    #     # Celery integration tests should use a different queue.
-    #     'task_default_queue': '__tests__',
-    # }
+    CELERY = {
+        # Local rabbitmq server
+        'broker_url': 'amqp://guest@localhost:5672//',
+        # Celery integration tests should use a different queue in case we have Celery workers
+        # running in development and happen to run tests at the same time.
+        'task_default_queue': '__tests__',
+    }
