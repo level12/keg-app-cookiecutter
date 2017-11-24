ca84910247c143e2cda5240103e8da5d6dd2aaa3
Randy Syring <randy@thesyrings.us>
2017-11-24 01:10:15 -0500

    add /ping-db
    fixes #31

--- {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_namespace}}/views/public.py
+++ {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_namespace}}/views/public.py
@@ -1,6 +1,7 @@
 import logging
 
 import flask
+from keg.db import db
 from keg.web import BaseView, rule
 
 public_bp = flask.Blueprint('public', __name__,)
@@ -18,3 +19,16 @@ class Hello(BaseView):
 
     def get(self, name='World'):
         self.assign('name', name)
+
+
+class PingDB(BaseView):
+    """ An endpoint our monitoring service can watch that, unlike Keg's /ping, will also
+        test connectivity to the DB before returning an "ok" message.
+    """
+    blueprint = public_bp
+
+    def get(self):
+        # We are happy if this doesn't throw an exception.  Nothing to test b/c we aren't sure
+        # there will be a user record.
+        db.engine.execute('select id from users limit 1').fetchall()
+        return '{} ok'.format(flask.current_app.name)
