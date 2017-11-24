538fb19fcf2653c172d827d134932da44f0f8558
Randy Syring <randy@thesyrings.us>
2017-11-24 01:49:59 -0500

    update Ansible config to be sufficient for typical app
    fixes #28 - Ansible config is insufficient

--- {{cookiecutter.project_dashed}}-src/ansible/files/{{cookiecutter.project_namespace}}-config.py
+++ {{cookiecutter.project_dashed}}-src/ansible/files/{{cookiecutter.project_namespace}}-config.py
@@ -13,9 +13,18 @@ class DeployedProfile(object):
     KEG_KEYRING_ENABLE = False
     KEG_LOG_SYSLOG_JSON = True
 
+    MAIL_DEFAULT_SENDER = 'devteam@level12.io'
 
-    MAIL_DEFAULT_SENDER = ''
+    CELERY = {
+        'broker_url': 'amqp://{{ app_rabbitmq_user }}:{{ app_rabbitmq_pass }}@localhost:5672/'
+            '{{ app_rabbitmq_vhost }}',
+        # Recycle workers at reasonable interval to work around possible memory leaks.
+        'worker_max_tasks_per_child': 2000,
+    }
 
     # Needed by at least KegAuth for sending emails from the command line
     SERVER_NAME = '{{ nginx_vhost_server_name }}'
+    # This is important so that generated auth related URLS are secure.  We have an SSL redirect
+    # but by the time that would fire, the key would have already been sent in the URL.
+    PREFERRED_URL_SCHEME = 'https'
 {% endraw %}
