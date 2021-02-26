319c57c5c9ccae0c18f8482d2c0c5cb26730e920
Matt Lewellyn <guruofgentoo@gmail.com>
2021-02-26 13:53:28 -0500

    use a timeout for ping request

--- {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_namespace}}/celery/tasks.py
+++ {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_namespace}}/celery/tasks.py
@@ -11,7 +11,7 @@ def ping_url(self, url, retry_wait_secs=1):
     log.info('pinging URL: %s', url)
 
     try:
-        requests.get(url)
+        requests.get(url, timeout=10)
     except requests.RequestException:
         log.exception('ping_url() encountered an exception')
         raise self.retry(countdown=retry_wait_secs)
