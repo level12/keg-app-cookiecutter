12e7564f78e0721f18629a95ed769d6222a44099
Randy Syring <randy@thesyrings.us>
2021-11-11 01:20:30 -0500

    remove default_assert_fn()
    Which was only used by grid assertion code that was removed a few commits ago.

--- {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_pymod}}/libs/testing.py
+++ {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_pymod}}/libs/testing.py
@@ -67,9 +67,6 @@ def mock_patch(*args, **kwargs):
     kwargs.setdefault('spec_set', True)
     return mock.patch(*args, **kwargs)
 
-def default_assert_fn(x, y):
-    assert x == y
-
 
 def query_to_str(statement, bind=None):
     """
