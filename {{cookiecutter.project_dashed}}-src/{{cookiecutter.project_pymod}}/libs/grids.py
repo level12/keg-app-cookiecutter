31d26bfb039124cdec4ab88db89459e55be278bb
Randy Syring <randy@thesyrings.us>
2021-11-11 14:41:00 -0500

    support grid header posts by default
    refs #150

--- {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_pymod}}/libs/grids.py
+++ {{cookiecutter.project_dashed}}-src/{{cookiecutter.project_pymod}}/libs/grids.py
@@ -2,6 +2,13 @@ import webgrid
 import webgrid.flask
 
 
+class GridManager(webgrid.flask.WebGrid):
+    args_loaders = (
+        webgrid.extensions.RequestArgsLoader,
+        webgrid.extensions.WebSessionArgsLoader,
+        webgrid.extensions.RequestFormLoader,
+    )
+
+
 class Grid(webgrid.BaseGrid):
-    manager = webgrid.flask.WebGrid()
-    session_on = True
+    manager = GridManager()
